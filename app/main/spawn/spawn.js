ec787b4b02bd3178b92fab4f1ba93ac130385e40
Sam Lee <sam49lee@gmail.com>
2019-05-06 16:47:21 -0400

    script spawner

---
+++ app/main/spawn/spawn.js
@@ -0,0 +1,44 @@
+const { spawn } = require('child_process');
+
+
+export default class Spawn{
+	constructor(){
+		this.scripts = [...arguments]
+	}
+	initiate_scripts(){
+		var processes = []
+		var script_handler = new ScriptHandler(this.scripts)
+		for(let script of script_handler){
+			processes.push(script)
+		}
+		return processes
+	}
+
+	get get_scripts(){
+		return initiate_scripts()
+	}
+}
+
+
+class ScriptHandler{
+	constructor(scripts_list){
+		this.scripts = scripts_list;
+
+	}
+	*[Symbol.iterator](){
+		for (let i of this.scripts){
+			const process = spawn('python', [i.path, i.args])
+			yield new Promise((resolve, reject) => {
+				process.stdout.on('data', data=>{
+					resolve(data.toString());
+				})
+				process.stdout.on('data', data=>{
+					resolve(data.toString());
+				})
+			})
+		}
+
+	}
+}
+
+
